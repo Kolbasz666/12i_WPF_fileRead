e86a3b19071d50e406ef37a0169f2aa23234e872
zsar.daniel <zsar.daniel@C19-TANAR>
2025-04-02 10:59:09 +0200

    Add project files.

---
+++ 12i_WPF_fileRead/FileManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace _12i_WPF_fileRead
+{
+    public class FileManager
+    {
+        private string fileName;
+
+        public FileManager(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public List<string> ReadFile() {
+            List<string> all = new List<string>();
+
+            try
+            {
+                all = File.ReadAllLines(fileName, Encoding.UTF8).Skip(1).ToList();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+            return all;
+        }
+
+    }
+}
