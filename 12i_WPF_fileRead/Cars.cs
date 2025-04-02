3eb87b23924d66fd43e57087dfb6947ee8f28d3a
zsar.daniel <zsar.daniel@C19-TANAR>
2025-04-02 11:13:13 +0200

    Szabolcs megjött

--- 12i_WPF_fileRead/Cars.cs
+++ 12i_WPF_fileRead/Cars.cs
@@ -8,6 +8,24 @@ namespace _12i_WPF_fileRead
 {
     public class Cars
     {
-
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int Power { get; set; }
+        public int Weight { get; set; }
+        public double PowerToWeight
+        {
+            get
+            {
+                return (double)Power / Weight;
+            }
+        }
+        public Cars(string line)
+        {
+            string[] temp = line.Split(';');
+            Manufacturer = temp[0];
+            Model = temp[1];
+            Power = int.Parse(temp[2]);
+            Weight = int.Parse(temp[3]);
+        }
     }
 }
